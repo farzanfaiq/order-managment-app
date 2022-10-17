b90ccd979cfca0f15cad6af0e82202317a32af2e
farjadakbar4 <farjadakbar4@gmail.com>
2022-10-17 17:39:39 +0500

    update

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -14,28 +14,7 @@ import FooterLayout from './FooterLayout';
 import SidebarLayout from './SidebarLayout';
 
 
-const {  Sider } = Layout;
-function getItem(label, key, icon, children) {
-  return {
-    key,
-    icon,
-    children,
-    label,
-  };
-}
-const items = [
-  getItem('Option 1', '1', <PieChartOutlined />),
-  getItem('Option 2', '2', <DesktopOutlined />),
-  getItem('User', 'sub1', <UserOutlined />, [
-    getItem('Tom', '3'),
-    getItem('Bill', '4'),
-    getItem('Alex', '5'),
-  ]),
-  getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-  getItem('Files', '9', <FileOutlined />),
-];
 const MainLayout = () => {
-  const [collapsed, setCollapsed] = useState(false);
   return (
     <Layout
       style={{
