285efc7d85dae8c816b81046ad982a653eefac4e
farjadakbar4 <farjadakbar4@gmail.com>
2022-10-20 11:28:31 +0500

    update

--- src/components/layout/BodyLayout.js
+++ src/components/layout/BodyLayout.js
@@ -1,17 +1,27 @@
 import React from "react";
 import { Header, Content } from "antd/lib/layout/layout";
-import { Breadcrumb } from "antd";
+import { Breadcrumb, Menu  } from "antd";
 import { Outlet } from "react-router-dom";
 
 const BodyLayout = () => {
+  const items1 = ['1', '2', '3'].map((key) => ({
+    key,
+    label: `nav ${key}`,
+  }));
+  
     return (
         <React.Fragment>
-        <Header
+        {/* <Header
           className="site-layout-background"
           style={{
             padding: 0,
           }}
-        />
+        /> */}
+
+        <Header className="header">
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} items={items1} />
+        </Header>
+        
         <Content
           style={{
             margin: '0 16px',
